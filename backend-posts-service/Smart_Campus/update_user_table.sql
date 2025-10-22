507f51ea2dfb5b69eb21440fe1d91e47b34d83d7
programer0189 <lzimu0189@gmail.com>
2025-10-22 11:51:13 +0800

    Delete backend-posts-service/Smart_Campus/update_user_table.sql

--- backend-posts-service/Smart_Campus/update_user_table.sql
+++
@@ -1,8 +0,0 @@
--- 更新user表，添加性别字段
-ALTER TABLE `user` ADD COLUMN `gender` VARCHAR(3) COMMENT '性别' AFTER `image`;
-
--- 更新现有用户的性别字段（可选，根据实际需求设置默认值）
--- UPDATE `user` SET `gender` = '男' WHERE `gender` IS NULL;
-
--- 验证表结构
-DESCRIBE `user`;
